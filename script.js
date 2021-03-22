57d81c242f5da582633ca5c3a11b017949069083
lucasmetron <lucasmetron@gmail.com>
2021-03-22 16:59:33 -0300

    filter

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ let alunos = [
     novoAluno("Mario",23),
     novoAluno("Jose",45),
     novoAluno("Marcia", 29),
-    novoAluno("Joao", 35)
+    novoAluno("Joao", 35),
 ];
+
+let jogadores = [
+    novoAluno("Lucas",23),
+    novoAluno("Lais",45),
+    novoAluno("Camila", 29),
+    novoAluno("Junior", 35)
+];
+
 
 function temMenos30(aluno){
     return aluno.idade < 30
@@ -18,8 +26,26 @@ function temMais30(aluno){
     return aluno.idade >= 30
 }
 
-let alunosMenos30 = alunos.filter(temMenos30)
-let alunosMaior30 = alunos.filter(temMais30)
+// function filtro(callback){
+//     let alunosFiltrados = []
+//     for(aluno of this){
+//         if (callback(aluno)){
+//             alunosFiltrados.push(aluno)
+//         }
+//     }
+//     return alunosFiltrados
+// }
 
-console.log(alunosMenos30)
-console.log(alunosMaior30)
+// alunos.filtro =filtro;
+
+console.log(alunos.filter(temMais30));
+console.log(jogadores.filter(temMais30));
+
+
+// console.log(jogadores.filtro(temMenos30))
+
+// let alunosMenos30 = alunos.filter(temMenos30)
+// let alunosMaior30 = alunos.filter(temMais30)
+
+// console.log(alunosMenos30)
+// console.log(alunosMaior30)
