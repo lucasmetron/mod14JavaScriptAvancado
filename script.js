a64e5fee0f3ab1cce813182c3002a8f8f9664fd3
lucasmetron <lucasmetron@gmail.com>
2021-03-23 14:52:58 -0300

    atribuição pro referencia array

--- script.js
+++ script.js
@@ -4,23 +4,33 @@ function novoAluno (nome,idade){
     return {nome, idade}
 }
 
+// let alunos = [
+//     novoAluno("Mario",23),
+//     novoAluno("Jose",45),
+//     novoAluno("Marcia",29),
+//     novoAluno("Joao",35),
+// ]
+
+// let alunoDaquiA5anos = (aluno) => {
+//     return aluno.idade +5
+// }
+// let maior30 = (aluno) => {
+//     if (aluno.idade > 30){
+//         return aluno
+//     } 
+// }
+
 let alunos = [
     novoAluno("Mario",23),
     novoAluno("Jose",45),
     novoAluno("Marcia",29),
     novoAluno("Joao",35),
 ]
+let novosAlunos = new Array(...alunos)
+let novosAlunos = alunos.slice()
+let novosAlunos = [...alunos]
+novosAlunos.push(novoAluno("teste", 25))
 
-
-let nomeIdade = (aluno) => aluno.nome + " tem "+ aluno.idade + " anos."
-
-let maior30 = (aluno) => {
-    if (aluno.idade > 30){
-        return aluno
-    } 
-}
-
-let filterEmap = alunos.filter(maior30).map(nomeIdade)
-
-console.log(filterEmap)
+console.log(alunos)
+console.log(novosAlunos)
 
