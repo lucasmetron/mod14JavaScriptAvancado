254841275fd12113270be750b7ddb2f6a743543c
lucasmetron <lucasmetron@gmail.com>
2021-03-23 13:19:39 -0300

    ainda estudando filter

--- script.js
+++ script.js
@@ -1,51 +1,35 @@
 
-let novoAluno = (nome, idade) => {
+
+function novoAluno (nome,idade){
     return {nome, idade}
 }
 
 let alunos = [
     novoAluno("Mario",23),
     novoAluno("Jose",45),
-    novoAluno("Marcia", 29),
-    novoAluno("Joao", 35),
-];
+    novoAluno("Marcia",29),
+    novoAluno("Joao",35),
+]
+
 
 let jogadores = [
-    novoAluno("Lucas",23),
-    novoAluno("Lais",45),
-    novoAluno("Camila", 29),
-    novoAluno("Junior", 35)
-];
+    novoAluno("lais",23),
+    novoAluno("lucas",45),
+    novoAluno("wagner",29),
+    novoAluno("bianca",35),
+]
 
-
-function temMenos30(aluno){
-    return aluno.idade < 30
+function temMenosde30(qualquercoisa){
+    return qualquercoisa.idade < 30
 }
 
-function temMais30(aluno){
-    return aluno.idade >= 30
+function temMaisde30(qualquercoisa){
+    return qualquercoisa.idade >= 30
 }
 
-// function filtro(callback){
-//     let alunosFiltrados = []
-//     for(aluno of this){
-//         if (callback(aluno)){
-//             alunosFiltrados.push(aluno)
-//         }
-//     }
-//     return alunosFiltrados
-// }
+let alunosMenosde30 = alunos.filter(temMenosde30)
+let jogadoresMais30 = jogadores.filter(temMaisde30)
 
-// alunos.filtro =filtro;
+console.log(alunosMenosde30)
+console.log(jogadoresMais30)
 
-console.log(alunos.filter(temMais30));
-console.log(jogadores.filter(temMais30));
-
-
-// console.log(jogadores.filtro(temMenos30))
-
-// let alunosMenos30 = alunos.filter(temMenos30)
-// let alunosMaior30 = alunos.filter(temMais30)
-
-// console.log(alunosMenos30)
-// console.log(alunosMaior30)
