46d9535b3def5ad85f6e58aea02d10e5e636f414
lucasmetron <lucasmetron@gmail.com>
2021-03-24 14:04:35 -0300

    função reduce feito manialmente

--- script.js
+++ script.js
@@ -9,11 +9,35 @@ let alunos = [
     novoAluno("João",35),
 ]
 
-let juntaLetra = (string, primeiroNome) =>{
-    return string + primeiroNome.nome;
+function somaNumeros (numero){
+    let total = null;
+
+    for (i=0; i < numero.length ; i++){
+        total += numero[i].idade
+    }
+    return "total de idade é "+total;
 }
 
-console.log(alunos.reduce(juntaLetra,""))
-console.log(alunos.reduce(juntaLetra,"Esses são todos nome juntos: "))
+function juntaNomes (pessoa){
+    let junto = "";
+    for (i=0; i <pessoa.length ; i++){
+        junto += pessoa[i].nome
+    }
+
+    return "Esses são todos nomes juntos "+junto
+}
+
+console.log(somaNumeros(alunos))
+console.log(juntaNomes(alunos))
 
 
+// function idadeTurma (total, obj){
+//     return  total + obj.idade
+// }
+// function nomeJunto (string, obj){
+//     return string + obj.nome;
+// }
+// let numeroSomados = alunos.reduce(idadeTurma,0)
+// let nomesJuntos = alunos.reduce(nomeJunto, "")
+// console.log(numeroSomados)
+// console.log(nomesJuntos)
